8fa12e701b4a57b26478d3506f6fb94d2bbbfdc6
Mateo Sabelli <mateosabelli4a@gmail.com>
2023-03-14 20:15:56 -0300

    first commit

---
+++ myapp/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Navbar from './components/Navbar/Navbar'
+
+function App() {
+
+  return (
+    <>
+    <Navbar/>
+    <div>
+      <h1 style={{backgroundColor: "", color:"gold"}}> React </h1>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ myapp/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return (
+        <> 
+        <div className="containerNav">
+            <ul className="containerNavItems">
+                <li className="navItems"><a href="" className="items">HOME</a></li>
+                <li className="navItems"><a href="" className="items">Me</a></li>
+                <li className="navItems"><a href="" className="items">SHOP</a></li>
+            </ul>
+        </div> 
+        </>
+    )
+}
+
+export default Navbar
